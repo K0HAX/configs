c5c93ab6f04ec41c791eb6c9b8d0dd1f82540847
Michael Englehorn <michael@englehorn.com>
2016-04-18 10:36:10 -0500

    Changed to .emacs.d format. Added gitignore.

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,22 @@
 ;; Automatic package install
-(setq package-list '(evil evil-leader evil-org git-commit smex ledger-mode magit magit-popup mediawiki org org-gcal powerline powerline-evil twittering-mode use-package))
+(setq package-list '(
+		     evil
+		     evil-leader
+		     evil-org
+		     git-commit
+		     smex
+		     ledger-mode
+		     magit
+		     magit-popup
+		     mediawiki
+		     org
+		     org-gcal
+		     org-bullets
+		     powerline
+		     powerline-evil
+		     twittering-mode
+		     use-package
+		     highlight-parentheses))
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -14,27 +31,14 @@
   (unless (package-installed-p package)
       (package-install package)))
 
-(require 'evil)
-(evil-mode 1)
 (setq ring-bell-function 'ignore)
 
 ;; My Packages
 (require 'powerline)
 (powerline-default-theme)
 
-;; Evil Mods
-(define-key evil-motion-state-map ";" 'smex)
-(define-key evil-motion-state-map ":" 'evil-ex)
-
-;; Ledger Mode
-(use-package ledger-mode
-	     :ensure t
-	     :init
-	     (setq ledger-clear-whole-transactions 1)
-
-	     :config
-	     (add-to-list 'evil-emacs-state-modes 'ledger-report-mode)
-	     :mode "\\.dat\\'")
+(load "~/.emacs.d/private/highlight-parentheses.el")
+(load "~/.emacs.d/private/org-bullets.el")
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -61,3 +65,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Enable evil mode LAST.
+(require 'evil)
+(evil-mode 1)
+
+;; Evil Mods
+(define-key evil-motion-state-map ";" 'smex)
+(define-key evil-motion-state-map ":" 'evil-ex)
+
+;; Ledger Mode
+(use-package ledger-mode
+	     :ensure t
+	     :init
+	     (setq ledger-clear-whole-transactions 1)
+
+	     :config
+	     (add-to-list 'evil-emacs-state-modes 'ledger-report-mode)
+	     :mode "\\.ldg\\'")
