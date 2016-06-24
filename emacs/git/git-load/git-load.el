6fb67de0f271bc5753c38ba067ba93dfe59a3e20
Michael Englehorn <michael@englehorn.com>
2016-06-24 15:23:00 -0500

    Moved git-load to a new repo. Updated config.org

--- emacs/git/git-load/git-load.el
+++ emacs/git/git-load/git-load.el
@@ -3,17 +3,13 @@
 (defvar git-load-default-directory "~/.emacs.d/git/")
 
 (defun git-load (git-load-repository git-load-package-name &optional directory)
-"
-This function takes a git repository location, package name and optionally a directory, and loads it into Emacs as a package.
-Think of it as making all of github into one huge MELPA repo.
-"
+  "
+  This function takes a git repository location, package name and optionally a directory, and loads it into Emacs as a package.
+  Think of it as making all of github into one huge MELPA repo.
+  "
   (unless directory (setq directory git-load-default-directory))
-  (if
-      (progn
-	(add-to-list 'load-path (concat directory git-load-package-name))
-	(load git-load-package-name))(git-repo? (concat directory git-load-package-name))
-    (progn
-      (let ((git-repo directory))
-	(git-clone git-load-repository))
-      (add-to-list 'load-path (concat directory git-load-package-name))
-      (load git-load-package-name))))
+  (unless (git-repo? (concat directory git-load-package-name))
+    (let ((git-repo directory))
+      (git-clone git-load-repository)))
+  (add-to-list 'load-path (concat directory git-load-package-name))
+  (load git-load-package-name))
