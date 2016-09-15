2afa9c6014721b1d71d7f8ae890b5de2ffc4c6b3
Michael Englehorn <michael@englehorn.com>
2016-09-15 14:02:03 -0500

    Updated config.org

--- emacs/init.el
+++ emacs/init.el
@@ -16,3 +16,32 @@
 	  (byte-compile-file "~/.emacs.d/config.el"))
       (load-file "~/.emacs.d/config.elc"))
   (org-babel-load-file "~/.emacs.d/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   (quote
+    (("register" "ledger ")
+     ("bal" "ledger -f %(ledger-file) bal Assets Liabilities")
+     ("bigbal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)"))))
+ '(newsticker-url-list-defaults
+   (quote
+    (("NY Times" "http://partners.userland.com/nytRss/nytHomepage.xml")
+     ("The Register" "http://www.theregister.co.uk/tonys/slashdot.rdf")
+     ("slashdot" "http://slashdot.org/index.rss" nil 3600))))
+ '(package-selected-packages
+   (quote
+    (evil-org evil-leader restart-emacs ox-twbs calfw org-bullets multi company helm hydra exec-path-from-shell web-mode multi-term w3m emms-info-mediainfo emms json-mode smex ledger-mode magit powerline-evil powerline twittering-mode erc-youtube erc-social-graph erc-image erc-hl-nicks erc-crypt erc-colorize git bbdb dtrt-indent htmlize rainbow-delimiters color-theme-solarized color-theme s use-package)))
+ '(send-mail-function (quote smtpmail-send-it))
+ '(twittering-use-master-password t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(trailing-whitespace ((t (:background "dim gray")))))
