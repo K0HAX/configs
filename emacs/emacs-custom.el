531e703691125e488a236059e1bb6bb42e7cef7c
Michael Englehorn <michael@englehorn.com>
2017-03-31 15:42:04 -0500

    Updated configs.

--- emacs/emacs-custom.el
+++ emacs/emacs-custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (govc evil-org evil-leader restart-emacs ox-twbs calfw org-bullets matrix-client company exec-path-from-shell web-mode multi-term w3m emms-info-mediainfo emms json-mode smex ledger-mode magit powerline-evil powerline twittering-mode erc-youtube erc-social-graph erc-image erc-hl-nicks erc-crypt erc-colorize git bbdb dtrt-indent htmlize rainbow-delimiters color-theme s use-package))))
+    (nxml-mode govc evil-org evil-leader restart-emacs ox-twbs calfw org-bullets matrix-client company exec-path-from-shell web-mode multi-term w3m emms-info-mediainfo emms json-mode smex ledger-mode magit powerline-evil powerline twittering-mode erc-youtube erc-social-graph erc-image erc-hl-nicks erc-crypt erc-colorize git bbdb dtrt-indent htmlize rainbow-delimiters color-theme s use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
