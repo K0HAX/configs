f59713715ac7291c9a25bf48e8bedf7e26f1320c
Michael Englehorn <michael@englehorn.com>
2017-08-13 19:32:37 -0500

    Updated init

--- emacs/init.el
+++ emacs/init.el
@@ -15,9 +15,3 @@
   (progn
     (org-babel-load-file "~/.emacs.d/config.org")
     (byte-compile-file "~/.emacs.d/config.el")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(trailing-whitespace ((t (:background "dim gray")))))
