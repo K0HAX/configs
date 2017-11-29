3249fddf30d30b516bce81843289378eda5c3272
Michael Englehorn <michael@englehorn.com>
2017-11-29 11:43:49 -0600

    Fixed patch appliers

--- patches/do-emacs-apollo.sh
+++ patches/do-emacs-apollo.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-patch -p0 < emacs-apollo-config.diff
+if [ $0 = './do-emacs-apollo.sh' ]; then
+parentdir=`dirname $PWD`
+patch -d$parentdir ./emacs/config.org ./patches/emacs-apollo-config.diff
+else
+    echo "This script must be run from the patches directory"
+fi
 
--- patches/do-emacs-nokeys.sh
+++ patches/do-emacs-nokeys.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-patch -p0 < emacs-nokeys-config.diff
+if [ $0 = './do-emacs-nokeys.sh' ]; then
+parentdir=`dirname $PWD`
+patch -d$parentdir ./emacs/config.org ./patches/emacs-nokeys-config.diff
+else
+    echo "This script must be run from the patches directory"
+fi
 
